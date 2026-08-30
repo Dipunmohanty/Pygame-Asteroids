54388cd24ee425352ca7a352b419396c31f6febc
Dipunmohanty <204915485+Dipunmohanty@users.noreply.github.com>
2026-08-30 18:53:16 +0000

    V:0.2.0 Added left-right rotations

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def main():
                     return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
 if __name__ == "__main__":
