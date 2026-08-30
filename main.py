cf54baecd3f659c32a1743bb5f93045c995a6c14
Dipunmohanty <204915485+Dipunmohanty@users.noreply.github.com>
2026-08-30 07:30:09 +0000

    V:0.1.0 Player creation and added shape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+from player import Player
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
@@ -8,14 +9,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0 
+    player = Player(
+        x = SCREEN_WIDTH / 2,
+        y = SCREEN_HEIGHT / 2
+    )
 
     running = True
     while running:
+        dt = clock.tick(60) / 1000
+        #print(dt)
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
